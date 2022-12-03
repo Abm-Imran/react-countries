187cc5073b99c31aeb92b7280e220f0abefdaafc
unknown <Abm-Imran>
2022-12-03 11:33:17 +0600

    Installed AXIOS to fetch data instead of FETCH and loaded all countries

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(()=>{
+        const url = 'https://restcountries.com/v3.1/all';
+        axios.get(url)
+        .then(data=> setCountries(data.data))
+    },[])
     return (
         <div>
-            <h1>Country list</h1>
+            <h1>Country Loaded: {countries.length}</h1>
         </div>
     );
 };
