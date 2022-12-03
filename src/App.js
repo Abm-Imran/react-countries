25336a282c6a0bc133860193ee4302b1f81a725b
unknown <Abm-Imran>
2022-12-03 11:52:45 +0600

    Nested Route setup done and set a dynamic link for each country name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Countries from './components/Countries/Countries';
+import CountryDetail from './components/CountryDetail/CountryDetail';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
@@ -12,7 +13,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/home' element={<Home></Home>} ></Route>
-        <Route path='/Countries' element={<Countries></Countries>} ></Route>
+        <Route path='/countries' element={<Countries></Countries>} >co
+          <Route path=':countryId' element={<CountryDetail></CountryDetail>} ></Route>
+        </Route>
         <Route path='/about' element={<About></About>} ></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    console.log(countries);
 
     useEffect(()=>{
         const url = 'https://restcountries.com/v3.1/all';
@@ -15,6 +17,12 @@ const Countries = () => {
     return (
         <div>
             <h1>Country Loaded: {countries.length}</h1>
+            {
+                countries.map(country => <Link 
+                    key={country.ccn3}
+                    to={'countries/'+country.name.common}
+                    >{country.name.common} </Link>)
+            }
         </div>
     );
 };
